c4f291a0546344194c102ed2f1db1e57cdd2d15a
Alex Stepanian <alexstepanian11@gmail.com>
2022-02-02 12:32:00 -0800

    add book seeds file

---
+++ seeds/index.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+const Book = require('../models/book');
+const axios = require('axios');
+
+mongoose.connect('mongodb://localhost:27017/book-club')
+.then(() => {
+    console.log('Connected to DB')
+}).catch(err => {
+    console.log('DB CONNECTION ERROR', err)
+});
+
+const books = [
+    'Don Quixote',
+    'Robinson Crusoe',
+    'Frankenstein',
+    'The Count of Monte Cristo',
+    'David Copperfield',
+    'Huckleberry Finn',
+    'The Picture of Dorian Gray',
+    'Treasure Island',
+    'The Hobbit',
+    'Ready Player One',
+    'The Great Gatsby',
+    'Nineteen Eighty-Four',
+    'Slaughterhouse-Five',
+    'The Lord Of The Rings',
+    'To Kill A Mockingbird',
+    'Gone Girl',
+    'The Curious Incident Of The Dog In The Night-Time',
+    'Life of Pi',
+    'The Book Thief',
+    'Catcher In the Rye'
+]
+
+const makeBook = async (bookTitle) => {
+    const response = await axios.get(`http://openlibrary.org/search.json?q=${bookTitle}`);
+    let author = 'J R R TOLKIEN';
+    let pageCount = 404;
+    let firstSentence = [];
+    let coverImageCode = 8406786;
+    //try to find data in the JSON response, look at first two books in the response and then resort to the default.
+    if (response.data.docs[0].author_name) {
+        author = response.data.docs[0].author_name[0]
+    } else {
+        author = response.data.docs[1].author_name[0]
+    }
+
+    if (response.data.docs[0].title) {
+        title = response.data.docs[0].title
+    } else {
+        title = response.data.docs[1].title
+    }
+
+    if (response.data.docs[0].number_of_pages_median) {
+        pageCount = response.data.docs[0].number_of_pages_median;
+    } else {
+        pageCount = response.data.docs[1].number_of_pages_median;
+    }
+
+    if (response.data.docs[0].first_sentence) {
+        firstSentence = response.data.docs[0].first_sentence;
+    } else {
+        firstSentence = [];
+    }
+
+    if (response.data.docs[0].cover_i) {
+        coverImageCode = response.data.docs[0].cover_i
+    } else {
+        coverImageCode = response.data.docs[1].cover_i;
+    }
+    //utilize the cover_i code to create image urls
+
+    let imageUrlM = 'https://covers.openlibrary.org/b/id/10872750-M.jpg';
+    let imageUrlL = 'https://covers.openlibrary.org/b/id/10872750-L.jpg';
+
+    imageUrlM = `https://covers.openlibrary.org/b/id/${coverImageCode}-M.jpg`;
+    imageUrlL = `https://covers.openlibrary.org/b/id/${coverImageCode}-L.jpg`;
+    const book = new Book({title, author, pageCount, firstSentence, imageUrlM, imageUrlL});
+    await book.save();
+}
+
+const deleteBooks = async () => {
+    const books = await Book.find({});
+    for (let book of books) {
+        await Book.findByIdAndDelete(book._id)
+    }
+}
+
+deleteBooks();
+
+for (let i = 0; i < books.length; i++) {
+    makeBook(books[i]);
+};
+console.log('seeding complete')
